a8e81b78b36cc1e00feea1f8002cf0db6df523a3
ErikASD <74484958+ErikASD@users.noreply.github.com>
2024-10-15 12:45:53 -0700

    Update withdraw.py

--- withdraw.py
+++ withdraw.py
@@ -7,6 +7,7 @@ NORMALIZER = 1000 * 1000 * 1000 * 1000
 
 class Withdraw:
 	def __init__(self, config):
+		self.ESTIMATE_LOOP = config["ESTIMATE_LOOP"]
 		self.ESTIMATE_RETRY_MAX = config["ESTIMATE_RETRY_MAX"]
 		self.ESTIMATE_PERCENT_DOWN = config["ESTIMATE_PERCENT_DOWN"]
 
@@ -16,41 +17,37 @@ class Withdraw:
 		deducted = user.balance_deduct(db, original_amount)
 		if not deducted:
 			return "not enough balance"
-		
-		################
-		retry_count = 0
-		transfer = xmr_wallet_rpc.transfer_no_relay(amount, address)
-		while not transfer and retry_count < self.ESTIMATE_RETRY_MAX:
-			amount = int(amount * (1-(self.ESTIMATE_PERCENT_DOWN/100))) #compounds, with default config goes down to 95% untill gives up, only happens in small cases
-			transfer = xmr_wallet_rpc.transfer_no_relay(amount, address)
-			retry_count += 1
-		
-		if not transfer:
-			user.balance_add(db, original_amount)
-			return "unable to estimate transfer"
-		#################
+		try:
+			if self.ESTIMATE_LOOP:
+				retry_count = 0
+				transfer = xmr_wallet_rpc.transfer_no_relay(amount, address)
+				while not transfer and retry_count < self.ESTIMATE_RETRY_MAX:
+					amount = int(amount * (1-(self.ESTIMATE_PERCENT_DOWN/100)))
+					transfer = xmr_wallet_rpc.transfer_no_relay(amount, address)
+					retry_count += 1
+				
+				if not transfer:
+					user.balance_add(db, original_amount)
+					return "unable to estimate transfer"
+			else:
+				transfer = xmr_wallet_rpc.transfer_no_relay(amount, address)
+				if not transfer:
+					user.balance_add(db, original_amount)
+					return "estimate transfer failed"
 
+			transfer2 = xmr_wallet_rpc.transfer_no_relay(amount - transfer["fee"], address) #sends adjusted amount with fee, perfect above 0.0002 XMR
+			if not transfer2:
+				user.balance_add(db, original_amount)
+				return "transfer failed"
 
-		"""
-		can replace code within the "#"s with code in "*"s for a more error prone estimate, usually reached when max withdraw occurs, but does not reduce amount with while loop:
-
-		*****************
-		transfer = xmr_wallet_rpc.transfer_no_relay(amount, address)
-		if not transfer:
-			user.balance_add(db, original_amount)
-			return "estimate transfer failed"
-		*****************
-
-
-		"""
-
-		transfer2 = xmr_wallet_rpc.transfer_no_relay(amount - transfer["fee"], address) #sends adjusted amount with fee, perfect above 0.0002 XMR
-		if not transfer2:
-			user.balance_add(db, original_amount)
-			return "transfer failed"
-
-		transfer_final = xmr_wallet_rpc.relay_tx(transfer2["tx_metadata"])
-		if not transfer_final:
+			transfer_final = xmr_wallet_rpc.relay_tx(transfer2["tx_metadata"])
+			if not transfer_final:
+				user.balance_add(db, original_amount)
+				return "relay failed"
+		except:
+			#When RPC relay throws an exception and quits before refund.
 			user.balance_add(db, original_amount)
 			return "relay failed"
+
+		db_withdraw = models.Transaction.create_withdraw(db, user.xmr_address_index, transfer2["amount"], transfer_final["tx_hash"])
 		return "transfered"
